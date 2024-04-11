726859b92423f846bb76d736e2e3a1003ca2a6ab
shaninalex <shanin.x@gmail.com>
2024-04-11 07:30:30 +0300

    fix: telescop search word

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,18 +1,19 @@
 require('telescope').setup {
-    defaults = {
-        mappings = {
-            i = {
-                ['<C-u>'] = false,
-                ['<C-d>'] = false,
-            },
-        },
-        file_ignore_patterns = {
-            '__pycache__',  -- python compiled binaries
-            'env',          -- python dependecies env folder
-            'node_modules', -- js libraries
-            'dist',         -- compilled package
-        }
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
     },
+    file_ignore_patterns = {
+      '__pycache__',  -- python compiled binaries
+      'env',          -- python dependencies env folder
+      'node_modules', -- js libraries
+      'dist',         -- compilled package
+    }
+  },
 }
+
 
 pcall(require('telescope').load_extension, 'fzf')
