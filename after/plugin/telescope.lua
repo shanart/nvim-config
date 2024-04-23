b7d7f5bb15b5560f2fe08059e0731c660bedec47
shaninalex <shanin.x@gmail.com>
2024-04-23 10:08:01 +0300

    setup lsps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,18 +1,18 @@
 require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
+    defaults = {
+        mappings = {
+            i = {
+                ['<C-u>'] = false,
+                ['<C-d>'] = false,
+            },
+        },
+        file_ignore_patterns = {
+            '__pycache__', -- python compiled binaries
+            'env',    -- python dependencies env folder
+            'node_modules', -- js libraries
+            'dist',   -- compilled package
+        }
     },
-    file_ignore_patterns = {
-      '__pycache__',  -- python compiled binaries
-      'env',          -- python dependencies env folder
-      'node_modules', -- js libraries
-      'dist',         -- compilled package
-    }
-  },
 }
 
 
